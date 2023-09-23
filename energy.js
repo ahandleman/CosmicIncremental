20170bef8798e50721077f02cff267a21a7b66ed
ahandleman <ahandleman@wustl.edu>
2023-09-23 17:56:47 -0500

    updated with background radiation

--- energy.js
+++ energy.js
@@ -7,11 +7,27 @@ let energyConversionRate = 1;
 let radiationCollectorCost = 10;
 let conversionAmplifierCost = 20;
 
-// Function to gather radiation
+let backgroundRadiation = 0;
+let backgroundRadiationCap = 100;
+let backgroundRadiationRate = 0.1; // This is the rate at which background radiation will accumulate per second
+
+// Function to accumulate background radiation
+function accumulateBackgroundRadiation() {
+    if (backgroundRadiation < backgroundRadiationCap) {
+        backgroundRadiation += backgroundRadiationRate;
+        if (backgroundRadiation > backgroundRadiationCap) backgroundRadiation = backgroundRadiationCap;
+    }
+}
+
+// Function to collect from the background radiation
 function gatherRadiation() {
-    if (radiation < radiationCap) {
-        radiation += radiationPerClick;
-        if (radiation > radiationCap) radiation = radiationCap; // Ensure we don't exceed the cap
+    if (radiation + backgroundRadiation <= radiationCap) {
+        radiation += backgroundRadiation;
+        backgroundRadiation = 0;
+    } else {
+        let excess = radiation + backgroundRadiation - radiationCap;
+        radiation = radiationCap;
+        backgroundRadiation = excess;
     }
 }
 
@@ -40,14 +56,37 @@ function buyConversionAmplifier() {
     }
 }
 
+function upgradeBackgroundRadiationCap() {
+    if (energy >= radiationCollectorCost) {
+        energy -= radiationCollectorCost;
+        backgroundRadiationCap *= 1.5; // Increase the cap
+        radiationCollectorCost *= 1.5; // Increase the cost for the next upgrade
+    }
+}
+
+// Function to upgrade the gathering rate
+function upgradeGatheringRate() {
+    if (energy >= conversionAmplifierCost) {
+        energy -= conversionAmplifierCost;
+        radiationPerClick *= 1.1; // Increase the gather rate
+        conversionAmplifierCost *= 1.5; // Increase the cost for the next upgrade
+    }
+}
+
+
 export {
+    accumulateBackgroundRadiation,
     gatherRadiation,
     convertRadiation,
     buyRadiationCollector,
     buyConversionAmplifier,
+    upgradeBackgroundRadiationCap,
+    upgradeGatheringRate,
     radiation,
     radiationCap,
     energy,
+    backgroundRadiation,
+    backgroundRadiationCap,
     radiationCollectorCost,
     conversionAmplifierCost
 };
